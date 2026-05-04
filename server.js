f34434fb91fbce3ead7988284dc3f59eb03641e6
yasnoponyal17 <snootfornoyeah@yandex.ru>
2026-05-04 20:46:35 +0300

    am am am

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.post('/decypher', uploadKeys, function (req, res, next) {
     res.send(decrypted.toString())
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Сервер запущен: http://localhost:3000')
 })
